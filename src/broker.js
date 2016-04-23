cce8ca5f921c06704285f40dabde524cb5bfa967
Ville Nukarinen <ville.nukarinen@gmail.com>
2016-04-23 17:43:35 +0300

    Switch socketcluster to socket.io

--- src/broker.js
+++
@@ -1,3 +0,0 @@
-export function run(broker) {
-	console.log('   >> Broker PID:', process.pid);
-}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,78 @@
-import {SocketCluster} from 'socketcluster';
+import socketio from 'socket.io';
+const io = socketio();
+import {types, channels} from '../protocol/protocol';
 
-const socketCluster = new SocketCluster({
-  workers: 1, // Number of worker processes
-  brokers: 1, // Number of broker processes
-  port: 8000, // The port number on which your server should listen
-  appName: 'opengamebox2-server', // A unique name for your app
+/**
+ * entity
+ * - id
+ * - imgHash
+ * - pos.x
+ * - pos.y
+ * - selectedClientId
+ */
+const entities = {};
 
-  /* A JS file which you can use to configure each of your
-   * workers/servers - This is where most of your backend code should go
-   */
-  workerController: __dirname + '/worker.js',
+let nextEntityId = 0;
 
-  /* JS file which you can use to configure each of your
-   * brokers - Useful for scaling horizontally across multiple machines (optional)
-   */
-  brokerController: __dirname + '/broker.js',
+io.on('connection', socket => {
+  console.log(`Client ${socket.remoteAddress} connected. (${socket.id})`);
 
-  // Whether or not to reboot the worker in case it crashes (defaults to true)
-  rebootWorkerOnCrash: true
+  
+  const arr = [];
+  Object.keys(entities).forEach(key => arr.push(entities[key]));
+  socket.emit(types.ENTITY_CREATE, arr);
+  socket.join(channels.GAME);
+  
+  socket.on(types.ENTITY_CREATE_REQUEST, entityArr => {
+    const createList = [];
+    entityArr.forEach(entity => {
+      const id = ++nextEntityId;
+      entity.id = id;
+      entities[id] = entity;
+      createList.push(entity);
+    });
+    io.to(channels.GAME).emit(types.ENTITY_CREATE, createList);
+  });
+
+  socket.on(types.ENTITY_DELETE_REQUEST, entityArr => {
+    const deleteList = [];
+    entityArr.forEach(entity => {
+      const id = entity.id;
+      if (entities[id]) {
+        delete entities[id];
+        deleteList.push({id});
+      }
+    });
+    io.to(channels.GAME).emit(types.ENTITY_DELETE, deleteList);
+  });
+
+  socket.on(types.ENTITY_SELECT_REQUEST, entityArr => {
+    const updateList = [];
+    Object.keys(entities).forEach(key => {
+      let entity = entities[key];
+
+      if (entity.selectedClientId === null || 
+            entity.selectedClientId === socket.id.substr(2)) {
+        entity.selectedClientId = entityArr.find(ent => entity.id === ent.id) ? socket.id.substr(2) : null;
+        updateList.push({id: entity.id, selectedClientId: entity.selectedClientId});
+      }
+    });
+
+    if (updateList.length !== 0) {
+      io.to(channels.GAME).emit(types.ENTITY_SELECT, updateList);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Client ${socket.remoteAddress} disconnected. (${socket.id})`);
+  });
 });
+
+
+io.listen(8000);
+
+
+
 
 /**
  * @param {Type}
